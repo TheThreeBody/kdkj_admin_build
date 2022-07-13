11292ecc800d9d133ff1415d98261908ee197b3c
TheThreeBody <29009714+TheThreeBody@users.noreply.github.com>
2022-07-13 13:22:55 +0800

    first commit after copyright changed

---
+++ precache-manifest.1a8d94d40ef788382b32954a339051e2.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a15dcbe1b2d36fb27af82f5d8e2c1151",
+    "url": "./index.html"
+  },
+  {
+    "revision": "279a7d7fabb33bc822e3",
+    "url": "./static/css/2.6fa48670.chunk.css"
+  },
+  {
+    "revision": "aaa485f424c496e1008a",
+    "url": "./static/css/3.50f53c61.chunk.css"
+  },
+  {
+    "revision": "289ba10990844e4407ce",
+    "url": "./static/css/main.bad7413f.chunk.css"
+  },
+  {
+    "revision": "279a7d7fabb33bc822e3",
+    "url": "./static/js/2.f1853983.chunk.js"
+  },
+  {
+    "revision": "aaa485f424c496e1008a",
+    "url": "./static/js/3.a01dc3b5.chunk.js"
+  },
+  {
+    "revision": "eeea26fe23c4d65e687d",
+    "url": "./static/js/4.2cca6354.chunk.js"
+  },
+  {
+    "revision": "289ba10990844e4407ce",
+    "url": "./static/js/main.0cfe0175.chunk.js"
+  },
+  {
+    "revision": "07a5d546f0018cb4bef2",
+    "url": "./static/js/runtime-main.90fa8998.js"
+  },
+  {
+    "revision": "553c69e9bcf06d9f6218730fc13fcdda",
+    "url": "./static/media/b1.553c69e9.jpg"
+  },
+  {
+    "revision": "defb98583257610e959baa67ab0fa53b",
+    "url": "./static/media/beauty.defb9858.jpg"
+  },
+  {
+    "revision": "b257fa9c5ac8c515ac4d77a667ce2943",
+    "url": "./static/media/default-skin.b257fa9c.svg"
+  },
+  {
+    "revision": "3c9ecb331155d54c5a1a5f0b35be36a9",
+    "url": "./static/media/y6oxFxU60dYw9khW6q8jGw.3c9ecb33.woff2"
+  }
+]);
